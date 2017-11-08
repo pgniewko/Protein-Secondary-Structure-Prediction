a6bfeaae9242ef7c178c646b8a3c1930d8ddd98f
Pawel Gniewek <gniewko.pablo@gmail.com>
2017-11-08 10:39:15 -0800

    Ends encoded with >>_<< character

--- src/proteinss.py
+++ src/proteinss.py
@@ -2,8 +2,7 @@ import sys
 
 SEQ_MAP = {"A":1,"R":2,"N":3,"D":4,"C":5,"Q":6,"E":7,"G":8,"H":9,\
            "I":10,"L":11,"K":12,"M":13,"F":14,"P":15,"S":16,"T":17,\
-           "W":18,"Y":19,"V":20,"B":21,"Z":22,"X":23,"-":24}
-
+           "W":18,"Y":19,"V":20,"B":21,"Z":22,"X":23,"-":24, "_"}
 
 class ProteinSS:
 
@@ -31,7 +30,7 @@ class ProteinSS:
         return aa_db_string, ss_db_string
 
     def _aa_substring(self, i, w):
-        aa_seq = w*"-" + self.get_seq() + w * "-"
+        aa_seq = w*"_" + self.get_seq() + w * "_"
         # i+w new index of ith aa
         # i+w-w 
         # i+w+w+1 : (i+w) new index; w-window width; +1 include the last one
